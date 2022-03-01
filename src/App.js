91456d6aa419138ea548ece0bfab2a2596cf0253
rakuten-JO <letian.xu@nextlogy.co.jp>
2022-03-01 18:50:05 +0900

    delete unnecessary branch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <CssBaseline />
+    {/* The rest of your application. Here should be router */}
+    <h1>h1 label</h1>
+  </React.Fragment>
   );
 }
 
