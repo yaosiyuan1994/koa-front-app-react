9ac4e42b28473f600f0abe53b95951ef897da8c4
rakuten-JO <letian.xu@nextlogy.co.jp>
2022-03-01 19:28:43 +0900

    add theme and router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+import Home from './views/pages/home';
+import SignIn from './views/pages/signIn';
+
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        // TODO Global Style here
+      `,
+      //theme color custom setting
+      // palette: {
+      //   primary: {
+      //     light: '#757ce8',
+      //     main: '#3f50b5',
+      //     dark: '#002884',
+      //     contrastText: '#fff',
+      //   },
+      //   secondary: {
+      //     light: '#ff7961',
+      //     main: '#f44336',
+      //     dark: '#ba000d',
+      //     contrastText: '#000',
+      //   },
+      // },
+    },
+  },
+});
+
+export default function App() {
   return (
-    <React.Fragment>
-    <CssBaseline />
-    {/* The rest of your application. Here should be router */}
-    <h1>h1 label</h1>
-  </React.Fragment>
+    <ThemeProvider theme={theme}>
+      <React.Fragment>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signIn" element={<SignIn />} />
+        </Routes>
+      </React.Fragment>
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ src/views/pages/home.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <>
+            <main>
+                <h2>Welcome to the homepage!</h2>
+                <p>You can do this, I believe in you.</p>
+            </main>
+        </>
+    );
+}
